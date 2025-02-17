d3162798fc914ec353152002832399ad5ca9c1c1
Juan Gomez <juanc1306gg@hotmail.com>
2025-02-17 13:40:12 -0500

    PrimeraClase

--- app/console.py
+++ app/console.py
@@ -1,1 +1,24 @@
 # TODO: Agrega el código de las clases de la interfaz de usuario aquí. Borra este comentario al terminar.
+from datetime import datetime
+class Note:
+    HIGH = "HIGH"
+    MEDIUM = "MEDIUM"
+    LOW = "LOW"
+
+    def __init__(self, code, tittle, text, importance):
+        self.code = code
+        self.tittle = tittle
+        self.text = text
+        self.importance = importance
+        self.creation_date = datetime.now()
+        self.tags = []
+
+    def add_tag(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return "Date: " + str(self.creation_date) + "\n" + self.tittle + ": " + self.text
+
+
+
